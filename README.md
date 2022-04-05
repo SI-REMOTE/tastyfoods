7f182a3dbb0f5fb58e54325180d0d4bc0d6fcb3f
ehisgodswill <ehisgodswill@gmail.com>
2022-04-05 13:53:45 +0100

    home update

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # tastymeals
 Food website
 #to use nodejs,
-copy nodemodule to js folder at ./js
-start server with nodemun with 
-node run server
+copy nodemodule to folder
+start server automatically with nodemon with 
+npm run server
+server at http://localhost:4000
--- server.js
+++ server.js
@@ -1,28 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const path = require('path');
 
+app.use(express.json());
+app.use(express.static(__dirname));
+app.use(cors());
 
 app.get('/' , function (req,res){
-    res.sendFile(path.resolve(__dirname, '../index.html'))
+    res.sendFile(path.resolve(__dirname, 'index.html'))
 })
 app.get('/login', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "../login.html"))
+    res.sendFile(path.resolve(__dirname, "login.html"))
 })
 app.get("/signup", (req,res)=>{
-    res.sendFile(path.response(__dirname,'../signup.html'))
+    res.sendFile(path.response(__dirname,'signup.html'))
 })
 app.get('/cart', function (req,res){
-    res.sendFile(path.resolve(__dirname,'../cart.html'))
+    res.sendFile(path.resolve(__dirname,'cart.html'))
 })
 app.get('/home', function (req,res){
-    res.sendFile(path.resolve(__dirname,'../index.html'))
+    res.sendFile(path.resolve(__dirname,'index.html'))
 })
 app.get('/menu', function (req,res){
-    res.sendFile(path.resolve(__dirname,'../menu.html'))
+    res.sendFile(path.resolve(__dirname,'menu.html'))
 })
 app.get('/order', function (req,res){
-    res.sendFile(path.resolve(__dirname,'../order.html'))
+    res.sendFile(path.resolve(__dirname,'order.html'))
 })
 
 if(process.env.NODE_ENV === 'production'){
